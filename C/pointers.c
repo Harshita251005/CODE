7fd19f0b384804198f0de2b61f60528194621d10
Harshita <harshita0601.be23@chitkara.edu.in>
2024-08-14 16:06:08 +0530

    done

--- C/pointers.c
+++ C/pointers.c
@@ -32,15 +32,18 @@
 // }
 
 
-// #include<stdio.h>
-// int main()
-// {
-//     int arr[5]={2,3,4,5,6};
-//     printf("positioon %d\n",arr[0]);
-//     printf("address of 1st %d\n",&arr[0]);
-//     printf("address of 2nd %d\n",&arr[1]);
-//     printf("address %d\n",*(arr + 1));
-//     return 0;
-// }
+#include<stdio.h>
+void changevalue(int* adress)
+{
+    *adress =345;
+}
+int main()
+{
+ int a =49,b=34;
+ printf("%d\n",a);
+ changevalue(&a); 
+ printf("%d",a);
+ return 0;
+}
 
 
