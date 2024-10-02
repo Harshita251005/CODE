e044f6768859c491ca2d8abcf5ea6fdc2f7e9992
Harshita <harshita0601.be23@chitkara.edu.in>
2024-10-02 13:43:40 +0530

    done

--- C++/opps.cpp
+++ C++/opps.cpp
@@ -1,17 +1,252 @@
-#include<iostream>
-using namespace std;
-class student{
-    public:
-    string name;
-    int age,roll_no;
-};
-int main()
-{
-    student s1;
-    s1.name="rohit";
-    s1.age=24;
-    s1.roll_no=23;
-    cout<<s1.name<<endl;
-    cout<<s1.age;
-    return 0;
-}
+// // #include <iostream>
+// // using namespace std;
+// // class student
+// // {
+// //     // public:
+// //     string name;
+// //     int age, roll_no;
+// //     string grade;
+// //     int pin;
+
+// // public:
+// //     void setname(string s)
+// //     {
+// //         // if (s.size() == 0)
+// //         // {
+// //         //     cout << "invalid";
+// //         //     return;
+// //         // }
+// //         name = s;
+// //     }
+// //     void setage(int a)
+// //     {
+// //         // if (age < 0 || age > 100)
+// //         // {
+// //         //     cout << "invalid age";
+// //         //     return;
+// //         // }
+// //         age = a;
+// //     }
+// //     void setroll_no(int r)
+// //     {
+// //         roll_no = r;
+// //     }
+// //     void setgrade(string g)
+// //     {
+// //         grade = g;
+// //     }
+// //     void getname()
+// //     {
+// //         cout << name << endl;
+// //     }
+// //     void getage()
+// //     {
+// //         cout << age << endl;
+// //     }
+// //     int getroll_no()
+// //     {
+// //         return roll_no;
+// //     }
+// //     void getgrade()
+// //     {
+// //         cout << grade;
+// //     }
+// // };
+// // int main()
+// // {
+// //     student s1;
+// //     // s1.name="rohit";
+// //     // s1.age=24;
+// //     // s1.roll_no=23;
+// //     // cout<<s1.name<<endl;
+// //     // cout<<s1.age;
+
+// //     s1.setname("harshita");
+// //     s1.setage(54);
+// //     s1.setroll_no(12);
+// //     s1.setgrade("A+");
+// //     s1.getname();
+// //     s1.getage();
+// //     cout << s1.getroll_no()<<endl;
+// //     s1.getgrade();
+// //     return 0;
+// // }
+
+// // #include <iostream>
+// // using namespace std;
+// // class a
+// // {
+// //     char c;
+// //      int b;
+// //     char g;
+
+// //     double j;
+// // };
+// // int main()
+// // {
+// //     a obj;
+// //     cout<<sizeof(obj)<<" ";
+// //     return 0;
+// // }
+
+// // dynamic memory allocation
+
+// // #include <iostream>
+// // using namespace std;
+// // class student
+// // {
+// // public:
+// //     string name;
+// //     int age;
+// //     string grade;
+// // };
+// // int main()
+// // {
+// //     student *s1 = new student;
+// //     (*s1).name = "rohit";
+// //     (*s1).age = 24;
+// //     (*s1).grade = "A+";
+// //     cout << s1->name << " ";
+// //     cout << s1->age << " ";
+// //     cout << s1->grade << " ";
+
+// //     return 0;
+// // }
+
+// // constructor
+// // #include <iostream>
+// // using namespace std;
+// // class custumer
+// // {
+// //     string name;
+// //     int account_no;
+// //     int balance;
+
+// // public:
+// //     // default constructor
+// //     custumer()
+// //     {
+
+// //         name = "paras";
+// //         account_no = 230;
+// //         balance = 10000;
+// //     }
+// //     // parameterized constructor
+// //     custumer(string a, int b, int c)
+// //     {
+// //         name = a;
+// //         account_no = b;
+// //         balance = c;
+// //     }
+// //     // constructor overloading
+// //     custumer(string a, int b)
+// //     {
+// //         name = a;
+// //         account_no = b;
+// //     }
+// //     // inline constructor
+// //     // inline custumer(
+// //     //     string a, int b, int c) : name(a), account_no(b), balance(c)
+// //     // {
+// //     // }
+// //     void display()
+// //     {
+// //         cout << name << " " << account_no << " " << balance << endl;
+// //     }
+// //     //copy constructor
+// //     custumer(custumer &b){
+// //         name=b.name;
+// //         account_no=b.account_no;
+// //     }
+// // };
+// // int main()
+// // {
+// //     custumer a1("harshita", 23, 200000);
+// //     a1.display();
+// //     custumer a2;
+// //     a2.display();
+// //     custumer a3("harsha", 13, 2000);
+// //     a3.display();
+// //     custumer a4(a3);
+// //     a4.display();
+// //     return 0;
+// // }
+
+// // destructor
+
+// // #include <iostream>
+// // using namespace std;
+// // class customer
+// // {
+// //     string name;
+// //     int *data;
+
+// // public:
+// //     customer()
+// //     {
+// //         name="4";
+// //         cout<<name<<endl;
+// //     }
+// //     // customer(){
+// //     //     name = "harshita";
+// //     //     data = new int;
+// //     //     *data = 10;
+// //     //     cout<<"constructor is called"<<endl;
+// //     // }
+// //     customer(string name)
+// //     {
+// //         this->name = name;
+// //         cout << name << endl;
+// //     }
+// //     // destructor
+// //     ~customer()
+// //     {
+// //         // delete data;
+// //         // cout<<"destructor is called"<<endl;
+// //         cout << name << endl;
+// //     }
+// // };
+// // int main()
+// // {
+// //     customer a1("1"), a2("2"), a3("3");
+// //     customer *a4 = new customer;
+// //     delete a4;
+// //     return 0;
+// // }
+
+
+// #include<iostream>
+// using namespace std;
+// class customer
+// {
+//     string name;
+//     int account_no,balance;
+//     int total_customer;
+
+//     public:
+//     customer(string name, int account_no, int balance)
+//     {
+//         this->name = name;
+//         this->account_no = account_no;
+//         this->balance = balance;
+//         total_customer++;
+//     }
+
+//     void display()
+//     {
+//         cout<<name<<" "<<account_no<<" "<<balance<<" "<<total_customer;
+//     }
+//     void display_total()
+//     {
+//         cout<<total_customer<<endl;
+//     }
+// };
+// int customer :: total_customer=0;
+
+//     int main()
+//     {
+//         customer a1("rohit",1,1000);
+//         customer a2("paras",2, 1800);
+//         a1.display();
+//         a2.display();
+//     }
--- C++/search.cpp
+++ C++/search.cpp
@@ -47,4 +47,378 @@
 //     return 0;
 // }
 
+// first and last position of an element
 
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int n;
+//     cin >> n;
+
+//     int arr[n];
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> arr[i];
+//     }
+
+//     cout << "Array elements: ";
+//     for (int i = 0; i < n; i++)
+//     {
+//         cout << arr[i] << " ";
+//     }
+//     cout << endl;
+
+//     int target;
+//     cin >> target;
+
+//     // Find the first occurrence of target
+//     int start = 0, end = n - 1, first = -1;
+//     while (start <= end)
+//     {
+//         int mid = start + (end - start) / 2;
+//         if (arr[mid] == target)
+//         {
+//             first = mid;
+//             end = mid - 1;
+//         }
+//         else if (arr[mid] < target)
+//         {
+//             start = mid + 1;
+//         }
+//         else
+//         {
+//             end = mid - 1;
+//         }
+//     }
+
+//     if (first != -1)
+//     {
+//         cout << "First occurrence of " << target << " is at index " << first << endl;
+//     }
+//     else
+//     {
+//         cout << "Target element not found in the array." << endl;
+//     }
+
+//     // Find the last occurrence of target
+//     start = 0;
+//     end = n - 1;
+//     int last = -1;
+//     while (start <= end)
+//     {
+//         int mid = start + (end - start) / 2;
+//         if (arr[mid] == target)
+//         {
+//             last = mid;
+//             start = mid + 1;
+//         }
+//         else if (arr[mid] < target)
+//         {
+//             start = mid + 1;
+//         }
+//         else
+//         {
+//             end = mid - 1;
+//         }
+//     }
+
+//     if (last != -1)
+//     {
+//         cout << "Last occurrence of " << target << " is at index " << last << endl;
+//     }
+//     else
+//     {
+//         cout << "Target element not found in the array." << endl;
+//     }
+
+//     return 0;
+// }
+
+// find the position of an element either it is there or not
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int n;
+//     cin >> n;
+
+//     int arr[n];
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> arr[i];
+//     }
+
+//     cout << "Array elements: ";
+//     for (int i = 0; i < n; i++)
+//     {
+//         cout << arr[i] << " ";
+//     }
+//     cout << endl;
+
+//     int target;
+//     cin >> target;
+
+//     int start = 0, end = n - 1, mid = -1;
+
+//     while (start <= end)
+//     {
+//         mid = start + (end - start) / 2;
+
+//         if (arr[mid] == target)
+//         {
+//             return mid;
+//         }
+//         else if (arr[mid] < target)
+//         {
+//             start = mid + 1;
+//         }
+//         else
+//         {
+//             end = mid - 1;
+//         }
+//     }
+
+//     if (mid != -1)
+//     {
+//         cout << "Index of " << target << " is at index " << mid << endl;
+//     }
+//     else
+//     {
+//         cout << start << endl;
+//     }
+
+//     return 0;
+// }
+
+// squrt
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int n;
+//     cin >> n;
+
+//     if (n == 0)
+//     {
+//         cout << "0" << endl;
+//         return 0;
+//     }
+
+//     int start = 1, end = n;
+
+//     while (start <= end)
+//     {
+//         int mid = start + (end - start) / 2;
+
+//         if (mid * mid == n)
+//         {
+//             cout << mid << endl;
+//             return 0;
+//         }
+
+//         if (mid * mid > n)
+//         {
+//             end = mid - 1;
+//         }
+//         else
+//         {
+//             start = mid + 1;
+//         }
+//     }
+//     cout << start - 1 << endl;
+
+//     return 0;
+// }
+
+// peak
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int n;
+//     cin >> n;
+
+//     int arr[n];
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> arr[i];
+//     }
+
+//     cout << "Array elements: ";
+//     for (int i = 0; i < n; i++)
+//     {
+//         cout << arr[i] << " ";
+//     }
+//     cout << endl;
+
+//     int start = 0, end = n - 1, mid;
+
+//     while (start <= end)
+//     {
+//         mid = end + ( start - end) / 2;
+
+//         if (arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1])
+//         {
+//             cout<<arr[mid];
+//             return 0;
+//         }
+//         else if (arr[mid] >arr[mid-1])
+//         {
+//             start = mid + 1;
+//         }
+//         else
+//         {
+//             end = mid - 1;
+
+//         }
+//     }
+//     cout<<"-1";
+
+//     return 0;
+// }
+
+// no. of times the targetv no come
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin >> n;
+
+//     int arr[n];
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> arr[i];
+//     }
+
+//     cout << "Array elements: ";
+//     for (int i = 0; i < n; i++)
+//     {
+//         cout << arr[i] << " ";
+//     }
+//     cout << endl;
+//     int target;
+//     cin >> target;
+//     int start = 0, end = n - 1, count = 0, mid;
+//     while (start <= end)
+//     {
+//         mid = start + (end - start) / 2;
+//         if (arr[mid] == target)
+//         {
+//             count = 1;
+//             int temp = mid - 1;
+//             while (temp >= start && arr[temp] == target)
+//             {
+//                 count++;
+//                 temp--;
+//             }
+//             temp = mid + 1;
+//             while (temp <= end && arr[temp] == target)
+//             {
+//                 count++;
+//                 temp++;
+//             }
+//             break;
+//         }
+//         else if (arr[mid] < target)
+//         {
+//             start = mid + 1;
+//         }
+//         else
+//         {
+//             end = mid - 1;
+//         }
+//     }
+//      return count;
+// }
+
+// search min in rotated array
+//  #include<iostream>
+//  using namespace std;
+//  int main()
+//  {
+//      int arr[];
+//      int n;
+//      int start=0, end =n-1,mid,ans=arr[0];
+//      while(start<=end)
+//      {
+//          mid=start+(end-start)/2;
+//          if(arr[mid]>=arr[0]){
+//              start=mid+1;
+//          }
+//          else{
+//          ans =arr[mid];
+//          end=mid-1;
+//          }
+//      }
+//      return ans;
+//  }
+
+// search in rotated array
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int n,nums[],target;
+//     int start=0,end=n-1,mid;
+//     while(start<=end)
+//     {
+//         mid=start+(end-start)/2;
+//         if(nums[mid]==target)
+//         {
+//             return mid;
+//         }
+//         else if(nums[mid]>=nums[0])
+//         {
+//             if(nums[mid]>=target && nums[start]<=target)
+//             {
+//                 end=mid-1;
+//             }
+//             else{
+//                 start=mid +1;
+//             }
+//         }
+//         else{
+//             if(nums[mid]<=target&& nums[end]>=target)
+//             {
+//                 start=mid+1;
+//             }
+//             else{
+//                 end=mid-1;
+//             }
+//         }
+//     }
+//     return -1;
+// }
+
+// kth missing positive no.
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int arr[], k;
+//     int start = 0, end = arr[] - 1, mid, ans;
+//     while (start <= end)
+//     {
+//         mid = start + (end - start) / 2;
+//         if (arr[mid] - mid - 1 >= k)
+//         {
+//             ans = mid;
+//             end = mid - 1;
+//         }
+//         else
+//         {
+//             start = mid + 1;
+//         }
+//     }
+
+//     return ans + k;
+// }
